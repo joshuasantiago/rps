83f02e9cb34d032ede912b1ec2632a38890f49d1
joshuasantiago <js520702@gmail.com>
2014-04-11 17:37:42 +0000

    Made a simple rps game

--- simple_rps.rb
+++ simple_rps.rb
@@ -1,4 +1,9 @@
 OUTCOMES = {"rock" => {"rock" => "tie", "paper" => "player 2", "scissors" => "player 1"},
-            "paper" => {},
-            "scissors" => {}
+            "paper" => {"paper" => "tie", "rock" => "player 1", "scissors" => "player 2"},
+            "scissors" => {"scissors" => "tie", "rock" => "player 2", "paper" => "player 1"}
              }
+
+computer_choice = ["rock", "paper", "scissors"].sample
+puts "Pick a choice: rock, paper, scissors"
+user_choice = gets.chomp
+puts OUTCOMES[user_choice][computer_choice]
